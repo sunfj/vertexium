fe267e7f7607e2dcf7cc6ed1a9f7f0301a673ec7
Matt Wizeman <matt.wizeman@gmail.com>
2017-05-17 14:46:29 -0400

    removing some debug code that I previously added

--- accumulo-blueprints/src/test/java/org/vertexium/accumulo/blueprints/AccumuloEdgeTest.java
+++ accumulo-blueprints/src/test/java/org/vertexium/accumulo/blueprints/AccumuloEdgeTest.java
@@ -3,11 +3,6 @@ package org.vertexium.accumulo.blueprints;
 import org.vertexium.accumulo.blueprints.util.AccumuloBlueprintsGraphTestHelper;
 import org.vertexium.blueprints.VertexiumBlueprintsEdgeTestBase;
 
-import java.net.InetAddress;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.concurrent.TimeUnit;
-
 public class AccumuloEdgeTest extends VertexiumBlueprintsEdgeTestBase {
     public AccumuloEdgeTest() {
         super(new AccumuloBlueprintsGraphTestHelper());
@@ -15,13 +10,6 @@ public class AccumuloEdgeTest extends VertexiumBlueprintsEdgeTestBase {
 
     @Override
     protected void setUp() throws Exception {
-        System.out.println("======================================");
-        final Instant startTime = Instant.now();
-        String hostName = InetAddress.getLocalHost().getHostName();
-        final Instant endTime = Instant.now();
-        System.out.printf("Method called, hostname %s, elapsed time: %d (ms)%n", hostName, TimeUnit.NANOSECONDS.toMillis(Duration.between(startTime, endTime).toNanos()));
-        System.out.println("======================================");
-
         super.setUp();
         ((AccumuloBlueprintsGraphTestHelper) this.graphTest).setUp();
     }
